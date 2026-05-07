992e9637fdacc3eb3db88a8e91fa1d0d5c889200
“Fei <xuf2@mskcc.org>
2026-05-07 14:12:01 -0400

    Updated cli.py

--- fgbio_postprocessing/cli.py
+++ fgbio_postprocessing/cli.py
@@ -4,12 +4,12 @@ from fgbio_postprocessing import simplex_filter
 
 
 @click.command()
-@click.option("--input_bam", required=True, help="Path to bam to be filtered")
-@click.option("--output_filename", required=False, help="Name of output bam")
-@click.option("--min_simplex_reads", required=False, default=3, help="Minimum number of simplex reads to pass filter")
-def filter_simplex(input_bam, output_filename, min_simplex_reads):
+@click.option("--input_bam", required=True, help="Path to simplex BAM to be filtered")
+@click.option("--duplex_bam", required=True, help="Path to duplex BAM used to identify duplex reads for removal")
+@click.option("--output_filename", required=False, help="Name of output filtered BAM")
+@click.option("--min_simplex_reads", required=False, default=3, type=int, help="Minimum number of simplex reads to pass filter")
+def filter_simplex(input_bam, duplex_bam, output_filename, min_simplex_reads):
     """
-    Filter bam file to only simplex reads with `min_simplex_reads` on one strand
+    Filter simplex BAM to remove reads that have a duplex complement in the duplex BAM.
     """
-    simplex_filter.filter_simplex(input_bam, output_filename, min_simplex_reads)
-    
+    simplex_filter.filter_simplex_with_duplex(input_bam, duplex_bam, output_filename, min_simplex_reads)
